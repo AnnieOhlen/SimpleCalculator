248e75d7740bc66e024d3129156f6d31d723552e
Annie <115146344+AnnieOhlen@users.noreply.github.com>
2023-07-04 22:05:30 +0200

    .
    .

---
+++ app/src/main/java/com/codeacademy/simplecalculator/MainActivity.java
@@ -0,0 +1,21 @@
+package com.codeacademy.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+//My imports.
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
