615847db3ca8f2709aadc9bfd2e82b7841d2251c
Annie <115146344+AnnieOhlen@users.noreply.github.com>
2023-07-05 07:54:09 +0200

    MainActivity.java
    Added Java variables.

--- app/src/main/java/com/codeacademy/simplecalculator/MainActivity.java
+++ app/src/main/java/com/codeacademy/simplecalculator/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 //My imports.
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //Check what final does.
+        final EditText firstNumber = findViewById(R.id.number1);
+        final EditText secondNumber = findViewById(R.id.number2);
+        RadioGroup operators = findViewById(R.id.operators);
+        RadioButton add = findViewById(R.id.add);
+        RadioButton subtract = findViewById(R.id.subtract);
+        Button equals = findViewById(R.id.equals);
+        TextView result = findViewById(R.id.result);
 
 
 
